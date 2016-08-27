f1e8a3206b012c3d7dc827f32182d745a8d90e20
Peter William Somers <petersomers2@gmail.com>
2016-08-27 18:19:27 -0400

    fixing indentation and adding controller reference

--- client/app.js
+++ client/app.js
@@ -1,87 +1,90 @@
 /*
-    This file is generated and updated by Sencha Cmd. You can edit this file as
-    needed for your application, but these edits will have to be merged by
-    Sencha Cmd when it performs code generation tasks such as generating new
-    models, controllers or views and when running "sencha app upgrade".
+ This file is generated and updated by Sencha Cmd. You can edit this file as
+ needed for your application, but these edits will have to be merged by
+ Sencha Cmd when it performs code generation tasks such as generating new
+ models, controllers or views and when running "sencha app upgrade".
 
-    Ideally changes to this file would be limited and most work would be done
-    in other places (such as Controllers). If Sencha Cmd cannot merge your
-    changes and its generated code, it will produce a "merge conflict" that you
-    will need to resolve manually.
-*/
+ Ideally changes to this file would be limited and most work would be done
+ in other places (such as Controllers). If Sencha Cmd cannot merge your
+ changes and its generated code, it will produce a "merge conflict" that you
+ will need to resolve manually.
+ */
 
 Ext.application({
-    name: 'Exercise',
+  name: 'Exercise',
 
-    requires: [
-        'Ext.MessageBox'
-    ],
+  requires: [
+    'Ext.MessageBox'
+  ],
 
-    views: [
-        'Main',
-        'Templates'
-    ],
+  views: [
+    'Main',
+    'Templates'
+  ],
 
-    models: [
-        'Exercise.model.Template',
-        'Exercise.model.Exercise',
-        'Exercise.model.TemplateExercise'
-    ],
+  models: [
+    'Exercise.model.Template',
+    'Exercise.model.Exercise',
+    'Exercise.model.TemplateExercise'
+  ],
 
-    controllers: [
-        'Main'
-    ],
+  controllers: [
+    'Main',
+    'Template'
+  ],
 
-    stores: [
-        'Template',
-        'TemplateExercise',
-        'Exercise'
-    ],
+  stores: [
+    'Template',
+    'TemplateExercise',
+    'Exercise',
+    'Workout',
+    'WorkoutSet'
+  ],
 
-    icon: {
-        '57': 'resources/icons/Icon.png',
-        '72': 'resources/icons/Icon~ipad.png',
-        '114': 'resources/icons/Icon@2x.png',
-        '144': 'resources/icons/Icon~ipad@2x.png'
-    },
+  icon: {
+    '57': 'resources/icons/Icon.png',
+    '72': 'resources/icons/Icon~ipad.png',
+    '114': 'resources/icons/Icon@2x.png',
+    '144': 'resources/icons/Icon~ipad@2x.png'
+  },
 
-    isIconPrecomposed: true,
+  isIconPrecomposed: true,
 
-    startupImage: {
-        '320x460': 'resources/startup/320x460.jpg',
-        '640x920': 'resources/startup/640x920.png',
-        '768x1004': 'resources/startup/768x1004.png',
-        '748x1024': 'resources/startup/748x1024.png',
-        '1536x2008': 'resources/startup/1536x2008.png',
-        '1496x2048': 'resources/startup/1496x2048.png'
-    },
+  startupImage: {
+    '320x460': 'resources/startup/320x460.jpg',
+    '640x920': 'resources/startup/640x920.png',
+    '768x1004': 'resources/startup/768x1004.png',
+    '748x1024': 'resources/startup/748x1024.png',
+    '1536x2008': 'resources/startup/1536x2008.png',
+    '1496x2048': 'resources/startup/1496x2048.png'
+  },
 
-    launch: function() {
-        // Destroy the #appLoadingIndicator element
-        Ext.fly('appLoadingIndicator').destroy();
+  launch: function() {
+    // Destroy the #appLoadingIndicator element
+    Ext.fly('appLoadingIndicator').destroy();
 
-        // Initialize the main view
-      /*var templateStore = Ext.getStore('templateStore');
-      var template = templateStore.getById('2');
-      var v = Ext.create('Exercise.view.Template',
-        {
-          data: template
+    // Initialize the main view
+    /*var templateStore = Ext.getStore('templateStore');
+     var template = templateStore.getById('2');
+     var v = Ext.create('Exercise.view.Template',
+     {
+     data: template
+     }
+     );
+     //v.setData(template);
+     Ext.Viewport.add(v);*/
+    Ext.Viewport.add(Ext.create('Exercise.view.Main'));
+  },
+
+  onUpdated: function() {
+    Ext.Msg.confirm(
+      "Application Update",
+      "This application has just successfully been updated to the latest version. Reload now?",
+      function(buttonId) {
+        if (buttonId === 'yes') {
+          window.location.reload();
         }
-        );
-      //v.setData(template);
-      Ext.Viewport.add(v);*/
-        Ext.Viewport.add(Ext.create('Exercise.view.Main'));
-    },
-
-    onUpdated: function() {
-        Ext.Msg.confirm(
-            "Application Update",
-            "This application has just successfully been updated to the latest version. Reload now?",
-            function(buttonId) {
-                if (buttonId === 'yes') {
-                    window.location.reload();
-                }
-            }
-        );
-    }
+      }
+    );
+  }
 });
